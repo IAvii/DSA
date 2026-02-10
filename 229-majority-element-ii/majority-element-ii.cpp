2f77310f4e2d63ac40982ce4137396400aaedd53
Avinash Ganore <avinashganore@gmail.com>
2026-02-10 12:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (82.90%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,16 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int count1=0,candidate1=0;
+        int count2=0,candidate2=0;
         int n = nums.size();
-        unordered_map<int,int> m;
-        vector<int> result;
-        
-        for (auto &el: nums) {
-            m[el]++;
-            if (m[el] == (n/3)+1) {
-                result.push_back(el);
+
+        for (int i=0; i<n; i++) {
+            if (count1==0 && nums[i]!= candidate2) {
+                count1=1;
+                candidate1=nums[i];
+            }
+            else if (count2==0 && nums[i]!= candidate1) {
+                count2=1;
+                candidate2=nums[i];
+            }
+            else if (candidate1 == nums[i]) {
+                count1++;
+            }
+            else if (candidate2 == nums[i]) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
         }
-        return result;
+
+        vector<int> res;
+        count1=0;
+        count2=0;
+        int threshold = n /3;
+        for (int i=0; i<n; i++) {
+            if (candidate1 == nums[i]) {
+                count1++;
+            } 
+            else if (candidate2 == nums[i]) {
+                count2++;
+            }
+        }
+        if (count1 > threshold) {
+            res.push_back(candidate1);
+        }
+        if (count2 > threshold) {
+            res.push_back(candidate2);
+        }
+        return res;
     }
 };
