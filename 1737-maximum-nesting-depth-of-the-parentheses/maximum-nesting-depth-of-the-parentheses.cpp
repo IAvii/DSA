898ff24e4532eb83eacd9409527571c57739c145
Avinash Ganore <23122157@pvgcoet.ac.in>
2026-06-13 13:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (23.56%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -5,19 +5,11 @@ public:
         int depth = 0;
         int count = 0;
         
-        for (int i =0; i<s.size(); i++) {
-            if (s[i] == '(' || s[i] == ')') {
-                if (s[i] == '(') {
-                count++;
-                } else if (s[i] == ')') {
-                    count--;
-                }
-                depth = max(depth, count);
-            } else {
-                continue;
-            }
+        for (int chr: s) {
+            if (chr == '(') count++;
+            else if (chr == ')') count--;
+            depth = max(depth, count);
         }
-
         return depth;
     }
 };
