3da75139c6c73fdfe11ddff1ee162642f1316884
Avinash Ganore <23122157@pvgcoet.ac.in>
2026-06-14 06:04:20 +0530

    Time: 11 ms (27.12%) | Memory: 13.3 MB (37.74%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -4,25 +4,15 @@ public:
         unordered_map<char,int> m = {{'I',1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 
         int result = 0;
-        int i =0;
-        while (i < s.size()-1) {
-            if (s[i] == 'I' && (s[i+1] == 'V' || s[i+1] =='X')) {
-                result += (m[s[i+1]] - m[s[i]]);
-                i += 2;
+  
+        for (int i=0; i< s.size()-1; i++) {
+            if (m[s[i]] < m[s[i+1]]) {
+                result -= m[s[i]];
             }
-            else if (s[i] == 'X' && (s[i+1] == 'L' || s[i+1] =='C')) {
-                result += (m[s[i+1]] - m[s[i]]);
-                i += 2;
-            }
-            else if (s[i] == 'C' && (s[i+1] == 'D' || s[i+1] =='M')) {
-                result += (m[s[i+1]] - m[s[i]]);
-                i += 2;
-            } else {
+            else {
                 result += m[s[i]];
-                i++;
             }
         }
-        result += m[s[i]];
-        return result;
+        return result += m[s.back()];
     }
 };
