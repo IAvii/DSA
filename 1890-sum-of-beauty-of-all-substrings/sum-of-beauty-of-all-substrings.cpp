aaa6b7b412eca0c7aff2acd5c0a629de2eaf909f
Avinash Ganore <23122157@pvgcoet.ac.in>
2026-06-24 11:13:03 +0530

    Time: 123 ms (35.01%) | Memory: 26 MB (31.29%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -5,18 +5,16 @@ public:
         int n = s.size();
 
         for (int i=0; i<n; i++) {
-            int arr[26] = {0};
+            unordered_map<char, int> m;
             for (int j=i; j<n; j++) {
-                arr[s[j] - 'a']++;
+                m[s[j]]++;
 
                 int minCount = INT_MAX;
                 int maxCount = INT_MIN;
 
-                for (int a: arr) {
-                    if (a != 0) {
-                        maxCount = max(maxCount, a); 
-                        minCount = min(minCount, a); 
-                    }
+                for (auto &a: m) {
+                    maxCount = max(maxCount, a.second); 
+                    minCount = min(minCount, a.second); 
                 }
                 bs += (maxCount - minCount);
             }
