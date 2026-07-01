3a5a10e39dfacbadcb4e4e0dd37ac5cf707547f1
Avinash Ganore <23122157@pvgcoet.ac.in>
2026-07-01 09:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (96.24%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int range = (1 << n);
+
+        vector<vector<int>> powerSet = {{}};
+
+        int k =1;
+        
+        for (int i =0; i<range; i++) {
+            vector<int> subset;
+
+            if ( i == (1 << k)) {
+                k++;
+            }
+
+            int j =0;
+            while (j < k) {
+                if (((i >> j) & 1) == 1) {
+                    subset.push_back(nums[j]);
+                }
+                j++;
+            }
+            if (!subset.empty()) {
+                powerSet.push_back(subset);
+            }
+        }
+        return powerSet;
+    }
+};
